709e65e7b724d796864f62d35cc4dd73241b3d71
sai2963 <venkatsai0629@gmail.com>
2023-10-13 05:52:05 -0700

    file updated

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-let cs=document.getElementById('connection-status');
-let ipA=document.getElementById('ipa');
-let Ns=document.getElementById('ns')
+let cs = document.getElementById("connection-status");
+let ipA = document.getElementById("ipa");
+let Ns = document.getElementById("ns");
 
-Ns.textContent='109:2458:259';
+function checkstatus() {
+  cs.textContent = "checking..";
+  if (navigator.onLine) {
+    fetch("https://api.ipify.org?format=json")
+      .then((response) => response.json())
+      .then((data) => {
+        cs.textContent = "Connected";
+        ipA.textContent = data.ip;
+        let connection = navigator.connection;
+        let network = connection ? connection.downlink + "Mbps" : "Unknown";
+        Ns.textContent = network;
+      })
+      .catch(() => {
+        cs.textContent = "disconneted";
+        ipA.textContent = "-";
+        Ns.textContent = "-";
+      });
+  } else {
+    cs.textContent = "disconneted";
+    ipA.textContent = "-";
+    Ns.textContent = "-";
+  }
+}
+
+window.addEventListener("load", checkstatus);
